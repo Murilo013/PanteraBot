68f9e748917a72503864217a38e2e8b5dd14b982
Murilo <muriloalessioamaral@gmail.com>
2025-05-02 18:35:43 -0300

    add port

--- src/config/index.js
+++ src/config/index.js
@@ -5,6 +5,12 @@ module.exports = {
     token: process.env.TOKEN
 }
 
+const port = process.env.PORT || 10000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
 
 
 
