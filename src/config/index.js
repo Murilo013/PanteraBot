de666ee54af9f8e9de2fe8ff8f27843aa934281c
Murilo <muriloalessioamaral@gmail.com>
2025-05-02 18:41:44 -0300

    fix index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,18 +1,9 @@
-require('dotenv').config();
-
-const express = require('express');
-const app = express(); 
+require('dotenv').config()
 
 
 module.exports = {
     token: process.env.TOKEN
 }
-
-const port = process.env.PORT || 10000;
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
 
 
 
